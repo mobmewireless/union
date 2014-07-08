536ad523ef34cf34074d49d4a9844dca69e3f978
Gokulnath M <gokulnath@mobme.in>
2014-07-08 18:50:25 +0530

    Devise fixes.
    Removed unused sessions_controller, application uses devise
    helpers everywhere and removed unused filters.

--- app/controllers/deployments_controller.rb
+++ app/controllers/deployments_controller.rb
@@ -1,11 +1,11 @@
 class DeploymentsController < ApplicationController
   def deploy
     deployment = Deployment.find(params[:id])
-    @job = deployment.deploy(session[:authenticated]['info']['email'].strip, admin: view_context.admin?)
+    @job = deployment.deploy(current_user.email, admin: view_context.admin?)
   end
 
   def setup
     deployment = Deployment.find(params[:id])
-    @job = deployment.setup(session[:authenticated]['info']['email'].strip, admin: view_context.admin?)
+    @job = deployment.setup(current_user.email, admin: view_context.admin?)
   end
 end
