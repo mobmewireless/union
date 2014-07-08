99b0a1ea5d300fc20dfe17c8c1b445d0692597a2
Gokulnath M <gokulnath@mobme.in>
2014-07-08 16:38:17 +0530

    Fix omniauth with devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks'}
+
   get 'boards/show'
 
   # Projects section - the heart of this 'project'. :)
@@ -50,14 +51,9 @@ Rails.application.routes.draw do
     match 'webhook' => 'api#webhook', via: [:get, :post]
   end
 
-  # Register
-  resources :identities
-
   # Auth routes
   match 'auth/:provider/callback' => 'sessions#create', via: [:get, :post]
   match 'auth/failure' => 'sessions#failure', via: [:get, :post]
-  # get 'logout' => 'sessions#destroy', as: 'logout'
-  # get 'login' => 'sessions#new', as: 'login'
 
   # Root '/' leads to the project index
   root to: 'projects#index'
